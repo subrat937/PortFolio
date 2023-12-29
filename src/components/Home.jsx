3c7295d9a2c2fc141a20a52add1c0de61c364603
subrat yadav <96230177+subrat937@users.noreply.github.com>
2023-12-29 21:59:13 +0530

    Add files via upload

---
+++ src/components/Home.jsx
@@ -0,0 +1,117 @@
+import React, { useRef } from "react";
+import { animate, motion } from "framer-motion";
+import Typewriter from "typewriter-effect";
+import { BsArrowUpRight, BsChevronDown } from "react-icons/bs";
+import me from "../assets/New-file (1).gif";
+
+const Home = ({ ratio }) => {
+  const clientCount = useRef(null);
+  const projectCount = useRef(null);
+
+  const animationClientsCount = () => {
+    animate(0, 0, {
+      duration: 1,
+      onUpdate: (v) => (clientCount.current.textContent = v.toFixed()),
+    });
+  };
+  const animationProjectsCount = () => {
+    animate(0, 5, {
+      duration: 1,
+      onUpdate: (v) => (projectCount.current.textContent = v.toFixed()),
+    });
+  };
+
+  const animations = {
+    h1: {
+      initial: {
+        x: "-100%",
+        opacity: 0,
+      },
+      whileInView: {
+        x: 0,
+        opacity: 1,
+      },
+    },
+    button: {
+      initial: {
+        y: "-100%",
+        opacity: 0,
+      },
+      whileInView: {
+        y: 0,
+        opacity: 1,
+      },
+    },
+  };
+  return (
+    <div id="home">
+      <section>
+        <div>
+          <motion.h1 {...animations.h1}>
+            Hi, I Am <br /> Subrat Yadav
+          </motion.h1>
+
+          <Typewriter
+            options={{
+              strings: ["A Developer", "A Designer", "A Creator"],
+              autoStart: true,
+              loop: true,
+              // cursor: "",
+              wrapperClassName: "typewriterpara",
+            }}
+          />
+
+          <div>
+            <a href="mailto:subratyadav204@gmail.com">Hire Me</a>
+            <a href="#work">
+              Projects <BsArrowUpRight />
+            </a>
+          </div>
+
+          <article>
+            <p>
+              +10
+              {ratio < 1000 && (
+                <motion.span
+                  whileInView={animationClientsCount}
+                  ref={clientCount}
+                ></motion.span>
+              )}
+            </p>
+            <span>Clients Worldwide</span>
+          </article>
+
+          <aside>
+            <article>
+              <p>
+                +
+                {ratio < 1000&& (
+                  <motion.span
+                    ref={projectCount}
+                    whileInView={animationProjectsCount}
+                  >
+                    500
+                  </motion.span>
+                )}
+              </p>
+              <span>Projects Done</span>
+            </article>
+
+            <article data-special>
+              <p>Contact</p>
+              <span>Subratyadav204@gmail.com</span>
+            </article>
+          </aside>
+        </div>
+      </section>
+      <section>
+       
+        <img id="me" src={me} alt="Subrat" />
+        
+      </section>
+      <BsChevronDown />
+    </div>
+  );
+};
+
+export default Home;
